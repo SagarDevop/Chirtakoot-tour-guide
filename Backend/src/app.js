773f22aab3a13ceb1ccbf6ecd062538a07e47b4c
SagarDevop <sagar.singh44818gmail.com>
2025-09-20 01:48:03 +0530

    allowing user

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,14 +3,24 @@ import authRoutes from './routes/authRoutes.js'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
+const allowedOrigins = [
+  process.env.CLIENT_URL,       
+  "http://localhost:5173"      
+];
+
 
 const app = express();
 
-app.use(cors(
-    {   origin: process.env.CLIENT_URL,
-        credentials: true,
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
     }
-))
+  },
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser())
 app.use('/api', authRoutes);
