c3a6d330e1854b33c4474c5b622919f16bb6b08f
SagarDevop <sagar.singh44818gmail.com>
2025-09-26 17:54:56 +0530

    fix: update email transport configuration to use environment variables

--- Backend/src/utils/sendEmail.js
+++ Backend/src/utils/sendEmail.js
@@ -2,13 +2,23 @@ import nodemailer from "nodemailer";
 
 export const sendEmail = async (to, subject, html) => {
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "Sagar.singh44818@gmail.com", 
-        pass: "wjyv znpq ondf qlky", 
-      },
-    });
+  const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // use SSL (465)
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS?.trim() // trim whitespace
+  },
+  logger: true,
+  debug: true,
+  tls: {
+    rejectUnauthorized: false
+  },
+  greetingTimeout: 10000,
+  connectionTimeout: 10000,
+  socketTimeout: 10000
+});
 
     await transporter.sendMail({
       from: `"Chitrakoot Yatra" <${process.env.EMAIL_USER}>`,
