84343e754a1fe821686aa6b42c620503338a45d8
SagarDevop <sagar.singh44818gmail.com>
2025-09-26 22:21:31 +0530

    fix: update API base URL to point to the correct deployment

--- Frontend/Chitrakoot-yatra/src/api.js
+++ Frontend/Chitrakoot-yatra/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://chitrakoot-yatra.vercel.app", 
+  baseURL: "https://chitrakoot-yatra.onrender.com", 
   withCredentials: true, 
 });
 
