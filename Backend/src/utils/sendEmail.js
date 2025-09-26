26f93b5f0fe966e4dccbf6ca14cf9a3d6448b901
SagarDevop <sagar.singh44818gmail.com>
2025-09-26 21:14:23 +0530

    fix: enhance email transport configuration with logging and debugging

--- Backend/src/utils/sendEmail.js
+++ Backend/src/utils/sendEmail.js
@@ -5,11 +5,13 @@ export const sendEmail = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
-  secure: true, // use SSL (465)
+  secure: true, 
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS?.trim() // trim whitespace
+    pass: process.env.EMAIL_PASS?.trim() 
+    
   },
+  
   logger: true,
   debug: true,
   tls: {
@@ -18,7 +20,11 @@ export const sendEmail = async (to, subject, html) => {
   greetingTimeout: 10000,
   connectionTimeout: 10000,
   socketTimeout: 10000
+  
 });
+console.log("Email User:", process.env.EMAIL_USER);
+console.log("Email Pass length:", process.env.EMAIL_PASS?.length);
+
 
     await transporter.sendMail({
       from: `"Chitrakoot Yatra" <${process.env.EMAIL_USER}>`,
